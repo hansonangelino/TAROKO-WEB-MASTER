0484290382293782f35e006faad9a29441c0a69a
Hanstopz Creation <www.hanstopz@gmail.com>
2020-05-03 15:14:35 +0700

    Asset

---
+++ Master Script.js
@@ -0,0 +1,19 @@
+function liked1() {	
+	document.getElementById('like_click1').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+function liked2() {	
+	document.getElementById('like_click2').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+function liked3() {	
+	document.getElementById('like_click3').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+function liked4() {	
+	document.getElementById('like_click4').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+function liked5() {	
+	document.getElementById('like_click5').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+function liked6() {	
+	document.getElementById('like_click6').style.cssText = 'filter: none; width: 24px; height: 20px; cursor: default; transition: 0.25s; opacity: 100%';
+}
+
